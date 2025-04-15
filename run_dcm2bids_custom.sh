57bb721ffafbdf05f10570430d8c35c8350e21a1
mchugomk <maureen.mchugo@gmail.com>
2025-04-15 14:31:15 -0600

    update study paths

--- run_dcm2bids_custom.sh
+++ run_dcm2bids_custom.sh
@@ -2,9 +2,9 @@
 
 # Run conda activate imaging first
 # Watch out for \r character at end of line on mac - may need dos2unix
-study_id=asdx
+study_id=valev
 study_path=/home/data/images/${study_id}
-subject_file=${study_path}/code/${study_id}_proc/${study_id}_dcm2bids_custom.csv
+subject_file=${study_path}/code/valev_proc/dcm2bids_custom.csv
 
 echo $subject_file
 {
@@ -12,9 +12,9 @@ read
 
 while IFS=, read -r subj_num ses_num scan_id ; do
 	echo "Reading Subject File: $subj_num $ses_num $scan_id "
-	config_file=${study_path}/code/${study_id}_proc/dcm2bids_json_files/dcm2bids_config_${study_id}_sub-${subj_num}_ses-0${ses_num}.json
+	config_file=${study_path}/code/valev_proc/dcm2bids_json_files/dcm2bids_config_${study_id}_sub-${subj_num}_ses-${ses_num}.json
 	
-	"$(dirname $0)"/dcm2bids_bic.sh -i $study_id -d  $scan_id -p $subj_num -s 0${ses_num} -c $config_file -m $study_path
+	"$(dirname $0)"/dcm2bids_bic.sh -i $study_id -d  $scan_id -p $subj_num -s ${ses_num} -c $config_file -m $study_path
 	echo
 done
 } < ${subject_file}
