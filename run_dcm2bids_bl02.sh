e3720190693fc2ea9f37c03544ba064318add311
mchugomk <maureen.mchugo@gmail.com>
2025-04-15 14:35:54 -0600

    Add dcm2bids config files for baseline 2 scans

---
+++ run_dcm2bids_bl02.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Run conda activate imaging first
+# Watch out for \r character at end of line on mac - may need dos2unix
+study_id=valev
+study_path=/home/data/images/${study_id}
+subject_file=${study_path}/code/valev_proc/dcm2bids_bl02.csv
+# config_file=${study_path}/code/valev_proc/dcm2bids_json_files/dcm2bids_config_${study_id}.json
+config_file=${study_path}/code/valev_proc/dcm2bids_json_files/dcm2bids_config_${study_id}_bl02.json
+echo $subject_file
+{
+read
+
+while IFS=, read -r subj_num ses_id scan_id ; do
+	echo "Reading Subject File: $subj_num $ses_id $scan_id "
+
+	"$(dirname $0)"/dcm2bids_bic.sh -i $study_id -d $scan_id -p $subj_num -s $ses_id -c $config_file -m $study_path
+	echo
+done
+} < ${subject_file}
